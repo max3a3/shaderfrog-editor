3989ef52c4af80ae2ef9e0bd277b852b827d8cf1
max3a3 <maxmax3a3@gmail.com>
2024-06-03 21:37:53 +0000

    set data

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,8 +1,9 @@
 import useSWR from 'swr';
 import { API } from './api';
 import { AssetsAndGroups } from '@editor/model/Asset';
+const  NO_DATA = {groups:{},assets:{}}
 
 export const useAssetsAndGroups = () => {
-  const { data } = useSWR(API.assets);
-  return data as AssetsAndGroups;
+  // const { data } = useSWR(API.assets);
+  return NO_DATA;
 };
